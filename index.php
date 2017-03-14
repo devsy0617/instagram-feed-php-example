2e0338851880af2fe9e2dab106d0ea63069762a7
suyeon <devsy0617@gmail.com>
2017-03-15 00:32:05 +0900

    크기별 이미지 호출 할 수 있는 함수 구현

--- index.php
+++ index.php
@@ -12,13 +12,33 @@ function fetchData($url)
     return $result;
 }
 
-
-$tmpData = "https://api.instagram.com/v1/users/self/media/recent/?access_token=".$CONFIG['ACCESS_TOKEN'];
+$tmpData = "https://api.instagram.com/v1/users/self/media/recent/?access_token=" . $CONFIG['ACCESS_TOKEN'];
 
 $result = fetchData($tmpData);
-$result = json_decode($result);
+$result = json_decode($result)->data;
 
+$data = getImages($result,'standard_resolution');
 
+// 크기별 이미지 호출 'standard_resolution','low_resolution','thumbnail'
+function getImages($data, $size)
+{
+    $imageData = array();
 
+    foreach ($data as $data_key => $data_value) {
+        $tmpImageObj = $data_value->images;
 
+        if($size == 'standard_resolution') {
+            $imageData[$data_key] = $tmpImageObj->standard_resolution->url;
+        }
+        else if($size == 'low_resolution') {
+            $imageData[$data_key] = $tmpImageObj->low_resolution->url;
+        }
+        else {
+            $imageData[$data_key] = $tmpImageObj->thumbnail->url;
+        }
+    }
+
+    return $imageData;
+}
+?>
 
