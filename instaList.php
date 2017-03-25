b22c888b775fc310de5a11ee972c4c32d8992ac4
suyeon <devsy0617@gmail.com>
2017-03-25 20:24:12 +0900

    리스트 출력

--- instaList.php
+++ instaList.php
@@ -1,11 +1,33 @@
+<?php
+session_start();
+include("class/CustomInstagram.php");
+
+$instaObj = new CustomInstagram();
+$imageList = $instaObj->getMyFeedTotalInfo('image','low_resolution');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    <link rel="stylesheet" type="text/css" href="css/flip.css">
 </head>
 <body>
-test
+
+<?php foreach ($imageList as $image) { ?>
+<div class="flip-container">
+    <div class="flipper">
+        <div class="front">
+            <img src="<?=$image?>">
+        </div>
+
+        <div class="back">
+            back
+        </div>
+    </div>
+</div>
+   <?php }?>
 
 </body>
 </html>
